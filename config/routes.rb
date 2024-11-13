2107a19a8a9a00ada6779c5bed426ad830063275
Octothorpe Brown <honella.davis@gmail.com>
2024-11-13 01:06:23 +0000

    Change user show page to show first post

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-    root "microposts#index"
+    root "users#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
